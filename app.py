6ac74d2a2dca94712842f80aca6f4930497a66cd
Jefåf Tian <jeff.tian@outlook.com>
2018-03-03 11:55:05 +0800

    展示灰度缩小版图片。

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ video_capture = cv2.VideoCapture(0)
 
 while True:
     ret, image = video_capture.read()
-    cv2.imshow('Test', image)
+    thumb = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    gray = cv2.cvtColor(thumb, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Test', gray)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
